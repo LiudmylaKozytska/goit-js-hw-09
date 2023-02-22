188585b8384fcf0be643212bf2956a44f339ed05
LiudmylaKozytska <112343159+LiudmylaKozytska@users.noreply.github.com>
2023-02-22 19:22:43 +0000

    Deploying to gh-pages from @ LiudmylaKozytska/goit-js-hw-09@b4e5fe97b91342390d140f6e470c3e74c9e16063 🚀

---
+++ 01-color-switcher.54ddbb51.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),r=document.querySelector("body");let o=null;e.addEventListener("click",(()=>{o=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled="true"})),t.addEventListener("click",(()=>{clearInterval(o),e.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.54ddbb51.js.map
